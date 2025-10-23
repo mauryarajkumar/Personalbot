3913a9ff3cff34414a753ec901dad7a73143783a
Rajkumar Maurya <166305560+mauryarajkumar@users.noreply.github.com>
2025-10-24 01:30:59 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
 
 app = FastAPI()
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="Use Here Your Api Key")
 
 # Allow requests from React (localhost:3000)
 app.add_middleware(
